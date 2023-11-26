8bd6b342000c92f52f3c77ef9147839ed1a3f14f
XaviBarragan <xbarragan73@gmail.com>
2023-11-26 21:34:51 +0100

    Añadir ejercicio-04

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navegacion from '../components/Ejercicio3/Navegacion'
 import Ejercicio1 from '../components/Ejercicio1/Ejercicio1'
 import Ejercicio2 from '../components/Ejercicio2/Ejercicio2'
 import Ejercicio3 from '../components/Ejercicio3/Ejercicio3'
+import Ejercicio4 from '../components/Ejercicio4/Ejercicio4'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import TituloPrincipal from '../components/TituloPrincipal'
@@ -16,6 +17,7 @@ export default function Home() {
         <Ejercicio1 />
         <Ejercicio2 />
         <Ejercicio3 />
+        <Ejercicio4 />
       </main>
       <Footer />
     </>
