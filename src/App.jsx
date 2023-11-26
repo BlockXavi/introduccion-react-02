5fa8d03a9b1c226a35974dfe153c3f06f0fc6475
XaviBarragan <xbarragan73@gmail.com>
2023-11-26 20:48:50 +0100

    Añadir ejercicio-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import Ejercicio1 from './components/Ejercicio1/Ejercicio1'
-import Ejercicio2 from './components/Ejercicio2/Ejercicio2'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import TituloPrincipal from './components/TituloPrincipal'
-import './index.css'
+// Importamos Routes y Route de react-router-dom
+import { Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 export default function App() {
+
   return (
-    <>
-      <Header />
-      <main>
-        <TituloPrincipal />
-        <Ejercicio1 />
-        <Ejercicio2 />
-      </main>
-      <Footer />
-    </>
+
+    // Definimos las Rutas con el componente padre Routes
+    <Routes>
+
+      {/*-- Definimos cada ruta de nuestra App con el componente Route. */}
+      
+      {/*-- Home: En este caso decimos que es index y no hace falta poner ninguna ruta. */}
+      <Route index element={<Home />} />
+
+      {/*-- About/Contact: Definimos la ruta que corresponde en "path" y */}
+      {/*-- en element ponemos el componente página que tiene que ser renderizado. */}
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+
+      {/*-- NotFound: Usar path="*" significa "coincidir con cualquier cosa", por lo que esta ruta */}
+      {/*-- actúa como una captura de todas las URL para las cuales no tenemos rutas explícitas definidas. */}
+      <Route path='*' element={<NotFound />} />
+
+    </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Importo BrowserRouter para gestionar las rutas
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Añadimos el BrowserRouter que envuelve la App para poder añadir las rutas de la app*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>  
   </React.StrictMode>
 )
